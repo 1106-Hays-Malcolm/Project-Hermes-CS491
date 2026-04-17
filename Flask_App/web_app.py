7601f96b5f9fca090828a248e844f512922b0b73
Malcolm Hays <mah2005atM@gmail.com>
2026-04-17 14:34:01 -0700

    Create basic flask app with HTML form

---
+++ Flask_App/web_app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def hello_world():
+    if request.method == "GET":
+        return render_template("index.html")
