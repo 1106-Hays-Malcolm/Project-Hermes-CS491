f583facd67f99145fa7f3fc681ae44dc262cf0df
Malcolm Hays <mah2005atM@gmail.com>
2026-04-18 23:45:31 -0700

    Send back client's question for testing

--- main.py
+++ main.py
@@ -13,13 +13,17 @@ def run_flask():
     web_app.init()
     web_app.app.run()
 
+
 def main():
     flask_thread = threading.Thread(target=run_flask, daemon=True)
     flask_thread.start()
 
+    # Listen for user input while Flask runs in the background
     while True:
         new_result = web_app.result_queue.get()
         print(new_result)
+        # Parrot back the user's question for testing
+        web_app.new_tokens_queue.put(new_result["question"])
 
 
 if __name__ == "__main__":
