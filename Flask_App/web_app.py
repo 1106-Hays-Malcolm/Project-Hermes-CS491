c006917b29cb5a57cba2505689864bfd7baf7b76
Malcolm Hays <mah2005atM@gmail.com>
2026-04-18 21:42:54 -0700

    Store user input in queue

--- Flask_App/web_app.py
+++ Flask_App/web_app.py
@@ -1,14 +1,22 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import queue
+
+
+def init():
+    global app
+    global result_queue
+
 
 app = Flask(__name__)
-
+result_queue = queue.Queue()
 
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
     if request.method == "POST":
-        print(request.form.get("mission-name"))
-        print(request.form.get("question"))
+        mission_name = request.form.get("mission-name")
+        question = request.form.get("question")
+        result_queue.put({"mission-name": mission_name, "question": question})
 
     return render_template("index.html")
